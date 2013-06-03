f12478632dc4ddfc04779c84af4472705aee3793
the-first-tiger@hotmail.de <tigger08>
2013-06-03 20:49:55 +0200

    Work on Own Database Handler

--- SponsorRunner Own Database Handling/Model/Database.cs
+++ SponsorRunner Own Database Handling/Model/Database.cs
@@ -8,9 +8,9 @@
 
     public class Database
     {
-        private string ConnectionString = "";
+        private string ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Alexander\SponsorRunnerOwnDbHandler.mdf;Integrated Security=True";
 
-        public Person GetPerson(int personId)
+        private Person GetPersonWithoutSponsors(int personId)
         {
             var person = new Person();
 
@@ -36,55 +36,224 @@
             }
 
             reader.Close();
-            
-            command = new SqlCommand(
+
+            connection.Close();
+
+            return person;
+        }
+
+        private void FillSponsors(Person person)
+        {
+            var connection = new SqlConnection(ConnectionString);
+
+            connection.Open();
+
+            var command = new SqlCommand(
                 "SELECT sponsorid, betrag FROM runnersponsor WHERE runnerid =" + person.PersonId,
                 connection);
 
-            reader = command.ExecuteReader();
+            var reader = command.ExecuteReader();
 
             while (reader.Read())
             {
                 person.Sponsors.Add(new RunnerSponsor
-                                   {
-                                       RunnerId = person.PersonId,
-                                       SponsorId = Convert.ToInt32(reader[0]),
-                                       Runner = person,
-                                       Betrag = Convert.ToInt32(reader[1])
-                                   });
+                {
+                    RunnerId = person.PersonId,
+                    SponsorId = Convert.ToInt32(reader[0]),
+                    Runner = person,
+                    Betrag = Convert.ToInt32(reader[1])
+                });
             }
 
             reader.Close();
 
             foreach (var runnerSponsor in person.Sponsors)
             {
-                command =
+                runnerSponsor.Sponsor = this.GetPersonWithoutSponsors(runnerSponsor.SponsorId);
+            }
+            
+            connection.Close();
+        }
+
+        public Person GetPerson(int personId)
+        {
+            var person = this.GetPersonWithoutSponsors(personId);
+            
+            this.FillSponsors(person);
+            
+            return person;
+        }
+
+        public void SavePerson(Person person)
+        {
+            if (DoesPersonExists(person.PersonId))
+            {
+                this.UpdatePerson(person);
+                return;
+            }
+
+            this.CreatePerson(person);
+        }
+
+        private void CreatePerson(Person person)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var command =
                     new SqlCommand(
-                        "SELECT personid, vorname, nachname, strasse, plz, ort FROM person WHERE personid = "
-                        + runnerSponsor.SponsorId,
+                        string.Format(
+                            "INSERT INTO person (vorname, nachname, strasse, plz, ort) OUTPUT INSERTED.IDENTITYCOL VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')",
+                            person.Vorname,
+                            person.Nachname,
+                            person.Strasse,
+                            person.Plz,
+                            person.Ort),
                         connection);
 
-                reader = command.ExecuteReader();
-
-                while (reader.Read())
+                person.PersonId = (int)command.ExecuteScalar();
+                
+                foreach (var runnerSponsor in person.Sponsors)
                 {
-                    runnerSponsor.Sponsor = new Person
-                                            {
-                                                PersonId = Convert.ToInt32(reader[0]),
-                                                Vorname = reader[1].ToString(),
-                                                Nachname = reader[2].ToString(),
-                                                Strasse = reader[3].ToString(),
-                                                Plz = reader[4].ToString(),
-                                                Ort = reader[5].ToString()
-                                            };
+                    if (this.DoesRunnerSponsorExists(runnerSponsor))
+                    {
+                        this.UpdateRunnerSponsor(runnerSponsor);
+                    }
+                    else
+                    {
+                        this.CreateRunnerSponsor(runnerSponsor);
+                    }
                 }
+            }
+        }
 
-                reader.Close();
+        private void UpdatePerson(Person person)
+        {
+            var connection = new SqlConnection(ConnectionString);
+
+            connection.Open();
+
+            var command =
+                new SqlCommand(
+                    string.Format(
+                        "UPDATE person SET  vorname = '{0}', nachname = '{1}', strasse = '{2}', plz = '{3}', ort = '{4}' WHERE personid = {5}",
+                        person.Vorname,
+                        person.Nachname,
+                        person.Strasse,
+                        person.Plz,
+                        person.Ort,
+                        person.PersonId),
+                    connection);
+
+            command.ExecuteNonQuery();
+
+            foreach (var runnerSponsor in person.Sponsors)
+            {
+                if (DoesRunnerSponsorExists(runnerSponsor))
+                {
+                    this.UpdateRunnerSponsor(runnerSponsor);
+                }
+                else
+                {
+                    if (!this.DoesPersonExists(runnerSponsor.SponsorId))
+                    {
+                        this.CreatePerson(runnerSponsor.Sponsor);
+                    }
+
+                    runnerSponsor.SponsorId = runnerSponsor.Sponsor.PersonId;
+
+                    this.CreateRunnerSponsor(runnerSponsor);   
+                }                
             }
 
             connection.Close();
+        }
 
-            return person;
+        private void CreateRunnerSponsor(RunnerSponsor runnerSponsor)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var command =
+                    new SqlCommand(
+                        string.Format(
+                            "INSERT INTO runnersponsor (runnerid, sponsorid, betrag) VALUES ({0}, {1}, {2})",
+                            runnerSponsor.RunnerId,
+                            runnerSponsor.SponsorId,
+                            runnerSponsor.Betrag),
+                        connection);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void UpdateRunnerSponsor(RunnerSponsor runnerSponsor)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var command =
+                    new SqlCommand(
+                        string.Format(
+                            "UPDATE runnersponsor SET betrag = {0} where runnerid = {1} and sponsorid = {2}",
+                            runnerSponsor.Betrag,
+                            runnerSponsor.RunnerId,
+                            runnerSponsor.SponsorId),
+                        connection);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private bool DoesRunnerSponsorExists(RunnerSponsor runnerSponsor)
+        {
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            var command =
+                new SqlCommand(
+                    string.Format(
+                        "SELECT runnerid FROM runnersponsor WHERE runnerid = {0} AND sponsorid = {1}",
+                        runnerSponsor.RunnerId,
+                        runnerSponsor.SponsorId),
+                    connection);
+
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    connection.Close();
+                    return true;
+                }
+
+                connection.Close();
+                return false;
+            }            
+        }
+
+        private bool DoesPersonExists(int personId)
+        {
+            var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            var command = new SqlCommand("SELECT personid FROM person WHERE personid =" + personId, connection);
+
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Close();
+                connection.Close();
+                return true;
+            }
+
+            reader.Close();
+            connection.Close();
+
+            return false;
         }
     }
 }
